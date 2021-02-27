b85b4d63c1a085568186a0f0756ee83a848c472b
magkamon <kamonmagdalena@gmail.com>
2021-02-27 15:21:19 +0100

    check gender view added

--- GenderDetectorWeb/src/main/java/detector/controller/GenderDetectorController.java
+++ GenderDetectorWeb/src/main/java/detector/controller/GenderDetectorController.java
@@ -4,9 +4,15 @@ import detector.dto.GenderForm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import detector.services.DetectorService;
+
+import javax.validation.Valid;
+
 
 @Controller
 @RequestMapping("gender")
@@ -23,10 +29,18 @@ public class GenderDetectorController {
     public String showGenderDetector(Model model){
         model.addAttribute(new GenderForm());
         model.addAttribute("nameCheckVariant", detectorService.getVariants());
-
-        return "OK";
+        return "check-gender-form";
     }
 
+    @PostMapping("/submit")
+    public String submitNeedRequestForm(@Valid @ModelAttribute("genderForm") GenderForm genderForm, BindingResult br, Model model) {
+        if (br.hasErrors()) {
+            model.addAttribute("nameCheckVariant", detectorService.getVariants());
+            return "check-gender-form";
+        } else {
+        return "OK";
+        }
+    }
 
 
 
--- GenderDetectorWeb/src/main/java/detector/dto/GenderForm.java
+++ GenderDetectorWeb/src/main/java/detector/dto/GenderForm.java
@@ -1,12 +1,29 @@
 package detector.dto;
 
 import detector.domain.NameCheckVariant;
+import javax.validation.constraints.Size;
 
 public class GenderForm {
 
+
+    @Size(min=3, message = "Name must be minimum of 3 characters")
     String name;
 
     NameCheckVariant nameCheckVariant;
 
+    public String getName() {
+        return name;
+    }
 
+    public NameCheckVariant getNameCheckVariant() {
+        return nameCheckVariant;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setNameCheckVariant(NameCheckVariant nameCheckVariant) {
+        this.nameCheckVariant = nameCheckVariant;
+    }
 }
